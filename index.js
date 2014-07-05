6cc46e4ecac65cd293be2691a56da906a9bcfb21
Michael Jade Ignacio <mikelignacio@gmail.com>
2014-07-05 21:26:03 +0800

    notice when user connects/disconnects

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
 	// res.send('<h1>Hello world</h1>');
 	// refactor to use sendfile instead
 	res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
 });
 
 http.listen(3000, function(){
